62eb8c3a6dba1bc1cc37b404d6a9034266269c43
Smrzygut <47782637+Smrzygut@users.noreply.github.com>
2021-04-12 17:36:09 -0400

    Added ALU tests

--- Tests/TestSignExt.v
+++ Tests/TestSignExt.v
@@ -37,4 +37,33 @@ B=10110;
 B=11111;
 #5;
 end
+
+reg [7:0] C, D;
+reg OP;
+wire [7:0] Cout3;
+
+ALU Behavioral_ALU (
+.a(C),
+.b(D),
+.out(Cout3),
+.op(OP)
+);
+initial begin
+C=01101101;
+D=00001100;
+OP=1;
+#5;
+C=00100001;
+D=00000100;
+OP=1;
+#5;
+C=01111101;
+D=01101100;
+OP=0;
+#5;
+C=00000001;
+D=00000000;
+OP=0;
+#5;
+end
 endmodule
